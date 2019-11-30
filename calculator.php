11040d3e5f3aa99e3cbccfb31f8cf30dc337d114
= <jmorillo@arborfintech.com>
2019-11-30 08:16:08 +0100

    Skeleton MathOperation class

---
+++ calculator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josem_000
+ * Date: 11/30/2019
+ * Time: 8:09 AM
+ */
+class MathOperation
+{
+
+    /**
+     * MathOperation constructor.
+     */
+    public function __construct()
+    {
+        echo 'Initializing calculator';
+    }
+}
+
+$calculator = new MathOperation();
